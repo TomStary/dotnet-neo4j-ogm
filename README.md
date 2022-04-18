25c36b808d896fb46e14fb91f53f77f9561100fc
Tomáš Starý <tomas.stary@icloud.com>
2022-04-18 09:33:49 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# dotnet-neo4j-ogm
+# Neo4j OGM for .NET
 
-OGM for Neo4j database in .NET
+| :bangbang: | This is proof of concept |
+| :--------: | :----------------------: |
+
+This library is an object-graph mapper with support for LINQ queries. The library
+is heavily inspired by the [Entity Framework](https://github.com/dotnet/efcore).
+
+## Getting Started
+
+In your `Program.cs` file register the necessary dependencies:
+
+```csharp
+builder.Services.AddNeo4jOGMFactory("connection/uri", IAuthToken, Assembly.GetAssembly(typeof(YourEntity)));
+```
+
+The IAuthToken is created from the `Neo4j.Driver` library. Navigate to [their repository](https://github.com/neo4j/neo4j-dotnet-driver) for more information.
+
+To use the library, inject the `SessionFactory` into your class and create a `Session` when needed.
+
+```csharp
+...
+using var session = sessionFactory.Create();
+var entities = session.Set<Entity>().ToListAsync();
+```
+
+More can be found in the [example project](https://github.com/TomStary/dotnet-neo4j-ogm-example).
+
+## Contributing
+
+You are welcome to contribute to this library, but before that, please read the "[How to contribute](https://github.com/TomStary/dotnet-neo4j-ogm/blob/main/.github/CONTRIBUTING.md)" page and [code of conduct](https://github.com/TomStary/dotnet-neo4j-ogm/blob/main/.github/CODE_OF_CONDUCT.md).
--- tests/Neo4j.OGM.Tests/Context/EntityGraphMapperTest.cs
+++ tests/Neo4j.OGM.Tests/Context/EntityGraphMapperTest.cs
@@ -58,6 +58,58 @@ public class EntityGraphMapperTest
     }
 
     [Fact]
+    public void MapTestIncomingRelationshipWithInfiniteDepth()
+    {
+        var assembly = new Mock<Assembly>();
+        assembly.Setup(a => a.GetTypes()).Returns(new[] { typeof(Person), typeof(Post) });
+
+        var metadata = new MetaData(assembly.Object);
+        var mappingContext = new MappingContext(metadata);
+        var entityGraphMapper = new EntityGraphMapper(mappingContext);
+
+        var model = new Post()
+        {
+            Title = "Hello World",
+            Author = new Person
+            {
+                Name = "John Doe",
+            }
+        };
+
+        var result = entityGraphMapper.Map(model, -1);
+
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<ICompilerContext>(result);
+        result.Compiler.UseStatementFactory(new RowStatementFactory());
+        Assert.NotNull(result.Compiler.GetAllStatements());
+    }
+
+    [Fact]
+    public void MapTestExistingRelationshipWithInfiniteDepth()
+    {
+        var assembly = new Mock<Assembly>();
+        assembly.Setup(a => a.GetTypes()).Returns(new[] { typeof(Person), typeof(Post) });
+
+        var metadata = new MetaData(assembly.Object);
+        var mappingContext = new MappingContext(metadata);
+        var entityGraphMapper = new EntityGraphMapper(mappingContext);
+
+        var model = new Person()
+        {
+            Id = 1,
+            Name = "John Doe",
+            Posts = new List<Post> { new Post() { Id = 2, Title = "Hello World" }, new Post() { Title = "Hello World 2" } },
+        };
+
+        var result = entityGraphMapper.Map(model, -1);
+
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<ICompilerContext>(result);
+        result.Compiler.UseStatementFactory(new RowStatementFactory());
+        Assert.NotNull(result.Compiler.GetAllStatements());
+    }
+
+    [Fact]
     public void MapTestRelationshipEntityWithInfiniteDepth()
     {
         var assembly = new Mock<Assembly>();
@@ -81,6 +133,37 @@ public class EntityGraphMapperTest
         Assert.NotNull(result.Compiler.GetAllStatements());
     }
 
+    [Fact]
+    public void MapTestRelationshipWithEntityWithInfiniteDepth()
+    {
+        var assembly = new Mock<Assembly>();
+        assembly.Setup(a => a.GetTypes()).Returns(new[] { typeof(Author), typeof(AuthorsRelationship), typeof(Note) });
+
+        var metadata = new MetaData(assembly.Object);
+        var mappingContext = new MappingContext(metadata);
+        var entityGraphMapper = new EntityGraphMapper(mappingContext);
+
+        var model = new Author
+        {
+            Name = "John Doe",
+            AuthorsRelationships = new List<AuthorsRelationship>
+            {
+                new AuthorsRelationship
+                {
+                    Note = new Note() { Title = "Hello World" }
+                },
+            },
+        };
+
+        ((List<AuthorsRelationship>)model.AuthorsRelationships)[0].Author = model;
+
+        var result = entityGraphMapper.Map(model, -1);
+
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<ICompilerContext>(result);
+        result.Compiler.UseStatementFactory(new RowStatementFactory());
+        Assert.NotNull(result.Compiler.GetAllStatements());
+    }
 
     [Fact]
     public void MapTestMultiRelationshipWithInfiniteDepth()
@@ -96,8 +179,9 @@ public class EntityGraphMapperTest
         {
             new AuthorsRelationship
             {
+                Id = 0,
                 Author = new Author() { Id = 1, Name = "John Doe" },
-                Note = new Note() { Title = "Hello World" }
+                Note = new Note() { Id = 2, Title = "Hello World" }
             },
             new AuthorsRelationship
             {
